84e40530fe1824b7195a87159be4fd29e7bf7041
Jay <kred110396@gmail.com>
2025-01-03 03:31:04 +0800

    feat: 🎸 add user info to map

--- src/components/LocationMap.vue
+++ src/components/LocationMap.vue
@@ -2,12 +2,17 @@
 import { ref, onMounted } from 'vue';
 import { map, tileLayer, marker, geoJSON } from 'leaflet';
 
+import { useAuthStore } from '@/stores/auth';
+import { storeToRefs } from 'pinia';
+
 const emit = defineEmits(['location-update']);
 
 const mapContainer = ref(null);
 const mapInstance = ref(null);
 
 const polygonData = ref([]);
+const authStore = useAuthStore();
+const { googleProfile, facebookProfile } = storeToRefs(authStore);
 
 const initMap = () => {
   mapInstance.value = map(mapContainer.value).setView([25.0374, 121.5167], 13)
@@ -21,9 +26,25 @@ const getCurrentLocation = () => {
     if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        const { latitude, longitude } = position.coords
-        mapInstance.value.setView([latitude, longitude], 15)
-        marker([latitude, longitude]).addTo(mapInstance.value)
+        const { latitude, longitude } = position.coords;
+        mapInstance.value.setView([latitude, longitude], 15);
+
+        marker([latitude, longitude])
+          .bindTooltip(`${googleProfile.value.name}<div class="w-12 h-12"><img src="${googleProfile.value.picture}" class="rounded-full border-2 border-white shadow-lg"></div>`, {
+            permanent: true,
+            direction: 'left',
+            offset: [0, 0],
+            className: 'flex flex-col items-center'
+          })
+          .addTo(mapInstance.value);
+
+        marker([latitude + 0.001, longitude + 0.001])
+        .bindTooltip(`${facebookProfile.value.name}<div class="w-12 h-12"><img src="${facebookProfile.value.picture}" class="rounded-full border-2 border-white shadow-lg"></div>`, {
+          permanent: true,
+          direction: 'right',
+          className: 'flex flex-col items-center'
+        })
+        .addTo(mapInstance.value)
 
         emit('location-update', {
           latitude,
